bf8a3ce67c63421b97522c15f2d32baef4e75cc1
Your Name <you@example.com>
2022-10-01 11:43:16 -0700

    Back to Basics

---
+++ reading_practice/l_t_p_part_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /* incl. header def standard (std) inputp (i) and output (o) */
+
+/**
+ * main - Entry point (means that main function is the entry point to the programme)
+ * 
+ */
+int main(void) /* void in brackets tells us that there is no argument/parameter passed to the function */
+{
+printf("Hello World!\n"); /*prints theh words in the bracets to "standard output" */
+return 0; /* integer (int) 0 is returned by programme, as main function sugg. that "int" is the data type returned to*/
+}
