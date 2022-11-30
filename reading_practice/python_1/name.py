3bade23524d7a1d6918ac6fef0d5d63490e290be
TsThMa <110995127+TsThMa@users.noreply.github.com>
2022-11-30 13:54:41 +0200

    flesh wound

---
+++ reading_practice/python_1/name.py
@@ -0,0 +1,7 @@
+# Ask the user to put in their name and assign it to variable named 'name'
+
+name = input("What is your name? ")
+
+#print out hello followed by the name that they typed input
+
+print("Hello" , name)
